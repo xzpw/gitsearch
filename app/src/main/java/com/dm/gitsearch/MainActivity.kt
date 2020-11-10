78028b685ce66fd37cf50e1b0208d667c915c26d
dm <xzpw123@gmail.com>
2020-11-10 18:20:26 +0200

    firs

---
+++ app/src/main/java/com/dm/gitsearch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dm.gitsearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
